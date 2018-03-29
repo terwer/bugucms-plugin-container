72e42e8aaf910dfd61c2053ce904097d7bbbb728
terwergreen <terwergreen@163.com>
2018-03-30 01:57:41 +0800

    Maven版本重新构建

---
+++ src/main/java/com/terwergreen/bugucms/BugucmsApplication.java
@@ -0,0 +1,12 @@
+package com.terwergreen.bugucms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugucmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugucmsApplication.class, args);
+	}
+}
