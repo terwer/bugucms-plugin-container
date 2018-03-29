c798e4476ef75c6ac7ea64bb388a1c32eefb106b
terwergreen <terwergreen@163.com>
2018-03-30 04:40:03 +0800

    Maven版本搭建完毕

--- src/main/java/com/terwergreen/bugucms/BugucmsApplication.java
+++ src/main/java/com/terwergreen/bugucms/BugucmsApplication.java
@@ -2,11 +2,14 @@ package com.terwergreen.bugucms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(value = {"com.terwergreen.front"})
 public class BugucmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BugucmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BugucmsApplication.class, args);
+    }
 }
---
+++ src/main/java/com/terwergreen/front/account/controller/AccountController.java
@@ -0,0 +1,43 @@
+package com.terwergreen.front.account.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.terwergreen.front.common.dto.RestResponseDTO;
+import com.terwergreen.front.common.util.RestResponseStates;
+import com.terwergreen.front.common.web.BGBaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.web.portlet.ModelAndView;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+/**
+ * Created by terwergreen on 2017-11-30.
+ */
+@Controller
+public class AccountController extends BGBaseController {
+
+    @RequestMapping(value = "/accountInfo", method = RequestMethod.GET)
+    public ModelAndView Hello(HttpServletRequest request) {
+        Map<String, String> resultMap = new HashMap<String, String>();
+        resultMap.put("flag", RestResponseStates.SUCCESS.getValue());
+        resultMap.put("msg", RestResponseStates.SUCCESS.getMsg());
+        // return super.processResultAll(resultMap);
+        return new ModelAndView();
+    }
+
+    @RequestMapping(value = "/activate", method = RequestMethod.GET)
+    @ResponseBody
+    public RestResponseDTO Hello2(HttpServletRequest request) {
+        RestResponseDTO restResponseDTO = new RestResponseDTO();
+        restResponseDTO.setFlag(RestResponseStates.SUCCESS.getValue());
+        restResponseDTO.setMsg(RestResponseStates.SUCCESS.getMsg());
+        return restResponseDTO;
+    }
+}
