809fa3469c0c49960c6bf821f0f42e8f5c25f259
terwergreen <terwergreen@163.com>
2018-03-30 01:36:14 +0800

    Gradle版本重新构建

--- README.md
+++
@@ -1,6 +0,0 @@
-# bugucms
-轻量级、插件化的内容管理系统
-
-# 相关技术
-Spring Boot  
-Gradle  
---
+++ src/test/java/com/terwergreen/bugucms/BugucmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.terwergreen.bugucms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BugucmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
