dd5156250f74b1c1c1280ecbcc48ec2b2363d3e2
terwergreen <terwergreen@163.com>
2018-04-10 18:31:01 +0800

    去掉MySQL时间默认值

--- src/main/ddl/bg_options.sql
+++ src/main/ddl/bg_options.sql
@@ -1,10 +1,33 @@
--- 站点配置表
-CREATE TABLE bg_options (
-  option_id bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '配置ID',
-  option_group varchar(64) NOT NULL DEFAULT '' COMMENT '配置组',
-  option_name varchar(64) NOT NULL DEFAULT '' COMMENT '配置名',
-  option_value longtext NOT NULL COMMENT '配置值',
-  PRIMARY KEY (option_id) USING BTREE,
-  UNIQUE KEY option_name (option_name) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='站点配置表';
+/*
+ Navicat Premium Data Transfer
 
+ Source Server         : tg-prd
+ Source Server Type    : MySQL
+ Source Server Version : 50556
+ Source Host           : 39.104.66.135:3306
+ Source Schema         : bugucms
+
+ Target Server Type    : MySQL
+ Target Server Version : 50556
+ File Encoding         : 65001
+
+ Date: 10/04/2018 18:27:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bg_options
+-- ----------------------------
+DROP TABLE IF EXISTS `bg_options`;
+CREATE TABLE `bg_options`  (
+  `option_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '配置ID',
+  `option_group` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '配置组',
+  `option_name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '配置名',
+  `option_value` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '配置值',
+  PRIMARY KEY (`option_id`) USING BTREE,
+  UNIQUE INDEX `option_name`(`option_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 9 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '站点配置表' ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
--- src/main/ddl/bg_users.sql
+++ src/main/ddl/bg_users.sql
@@ -1,17 +1,41 @@
--- 用户信息表
-CREATE TABLE `bg_users` (
-  `user_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-  `user_name` varchar(60) NOT NULL DEFAULT '' COMMENT '登录名',
-  `password` varchar(64) NOT NULL DEFAULT '' COMMENT '密码',
-  `nick_name` varchar(50) NOT NULL DEFAULT '' COMMENT '昵称',
-  `user_profile` varchar(250) NOT NULL DEFAULT '' COMMENT '简介',
-  `user_url` varchar(100) NOT NULL DEFAULT '' COMMENT '首页链接',
-  `mobile` varchar(11) NOT NULL DEFAULT '' COMMENT '手机',
-  `email` varchar(100) NOT NULL DEFAULT '' COMMENT '邮箱',
-  `activation_key` varchar(60) NOT NULL DEFAULT '' COMMENT '激活码',
-  `status` int(10) NOT NULL DEFAULT '0' COMMENT '用户状态(1:启用,0:禁用)',
-  `user_registered` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : tg-prd
+ Source Server Type    : MySQL
+ Source Server Version : 50556
+ Source Host           : 39.104.66.135:3306
+ Source Schema         : bugucms
+
+ Target Server Type    : MySQL
+ Target Server Version : 50556
+ File Encoding         : 65001
+
+ Date: 10/04/2018 18:27:46
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for bg_users
+-- ----------------------------
+DROP TABLE IF EXISTS `bg_users`;
+CREATE TABLE `bg_users`  (
+  `user_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `user_name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '登录名',
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '密码',
+  `nick_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '昵称',
+  `user_profile` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '简介',
+  `user_url` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '首页链接',
+  `mobile` varchar(11) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '手机',
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '邮箱',
+  `activation_key` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL DEFAULT '' COMMENT '激活码',
+  `status` int(10) NOT NULL DEFAULT 0 COMMENT '用户状态(1:启用,0:禁用)',
+  `user_registered` datetime NOT NULL COMMENT '注册时间',
   PRIMARY KEY (`user_id`) USING BTREE,
-  KEY `user_login_key` (`user_name`) USING BTREE,
-  KEY `user_nickname` (`nick_name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 CHECKSUM=1 DELAY_KEY_WRITE=1 ROW_FORMAT=DYNAMIC COMMENT='用户信息表';
+  INDEX `user_login_key`(`user_name`) USING BTREE,
+  INDEX `user_nickname`(`nick_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户信息表' ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
