55ab1c7e538e50749e564d975b421c38e409515b
Terwer <youweics@sina.com>
2018-12-04 13:20:17 +0800

    修复项目类型

--- src/main/java/com/terwergreen/bugucms/BugucmsApplication.java
+++ src/main/java/com/terwergreen/bugucms/BugucmsApplication.java
@@ -2,6 +2,7 @@ package com.terwergreen.bugucms;
 
 import com.terwergreen.bugucms.container.BugucmsPluginManager;
 import com.terwergreen.plugins.PluginInterface;
+import com.terwergreen.util.PropertyUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,17 +22,7 @@ public class BugucmsApplication {
 
     public static void main(String[] args) {
         // 获取项目的配置类型
-        // URL url = Thread.currentThread().getContextClassLoader().getResource("application.properties");
-        // if (url == null) {
-        //    throw new RuntimeException("Cannot find application.properties on classpath");
-        // }
-        // String file = url.getFile();
-
-        // String appFilename = new File("application.properties").getPath();
-        // Configuration config = PropertyUtil.readConfig(appFilename);
-        // WebApplicationType applicationType =config.getString("bugucms.web.application-type").equals("servlet") ? WebApplicationType.SERVLET : WebApplicationType.REACTIVE;
-
-        WebApplicationType applicationType = WebApplicationType.SERVLET;
+        WebApplicationType applicationType = PropertyUtil.readProperty("bugucms.web.application-type").equals("servlet") ? WebApplicationType.SERVLET : WebApplicationType.REACTIVE;
         //设置应用类型
         SpringApplication springApplication = new SpringApplication(BugucmsApplication.class);
         springApplication.setWebApplicationType(applicationType);
@@ -41,25 +32,25 @@ public class BugucmsApplication {
     @Bean
     public ApplicationRunner run() {
         return new ApplicationRunner() {
-//            @Value("${bugucms.plugin-switch}")
-//            private boolean pluginSwitch;
-//
-//            @Value("${bugucms.web.application-type}")
-//            private String applicationType;
-//
-//            @Autowired(required = false)
-//            private BugucmsPluginManager bugucmsPluginManager;
+            @Value("${bugucms.plugin-switch}")
+            private boolean pluginSwitch;
+
+            @Value("${bugucms.web.application-type}")
+            private String applicationType;
+
+            @Autowired(required = false)
+            private BugucmsPluginManager bugucmsPluginManager;
 
             @Override
             public void run(ApplicationArguments args) {
-//                // 输出插件信息
-//                if (pluginSwitch) {
-//                    List<PluginInterface> plugins = bugucmsPluginManager.getExtensions(PluginInterface.class);
-//                    logger.info(String.format("共找到%d个插件", plugins.size()));
-//                    plugins.forEach(c -> logger.info("插件:" + c.getClass().getName() + ":" + c.identify()));
-//                }
-//                logger.info("容器启动完毕，项目类型为：" + applicationType.toUpperCase());
-                logger.info("容器启动完毕");
+                // 输出插件信息
+                if (pluginSwitch) {
+                    List<PluginInterface> plugins = bugucmsPluginManager.getExtensions(PluginInterface.class);
+                    logger.info(String.format("共找到%d个插件", plugins.size()));
+                    plugins.forEach(c -> logger.info("插件:" + c.getClass().getName() + ":" + c.identify()));
+                }
+                logger.info("容器启动完毕，项目类型为：" + applicationType.toUpperCase());
+                logger.info("SUCCESS");
             }
         };
     }
