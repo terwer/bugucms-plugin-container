45808a724e33a0dcacdffadaaa722c49d60e9ea6
terwer <youweics@sina.com>
2018-12-10 02:45:58 +0800

    添加异常处理

---
+++ src/main/java/com/terwergreen/bugucms/util/Constants.java
@@ -0,0 +1,15 @@
+package com.terwergreen.bugucms.util;
+
+/**
+ * 默认常量定义
+ */
+public class Constants {
+    /**
+     * 默认页码
+     */
+    public static final int DEFAULT_PAGE_NUM = 1;
+    /**
+     * 默认每页展示数目
+     */
+    public static final int DEFAULT_PAGE_SIZE = 10;
+}
