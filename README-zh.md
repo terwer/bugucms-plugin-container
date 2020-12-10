44da108d2f03eff6c15a7fff65d7d81e45e726c7
Terwer <youweics@sina.com>
2020-12-10 17:46:44 +0800

    调整maven下载地址国内并简化构建脚本

--- README-zh.md
+++ README-zh.md
@@ -6,12 +6,6 @@
 
 ```
 cd $WORKSPACE
-cd src/main/webapp
-npm get registry
-npm install
-npm run build
-
-cd $WORKSPACE
-mvnw clean package
-mvnw spring-boot:run
+chmod +x run.sh
+./run.sh
 ```
---
+++ run.sh
@@ -0,0 +1,26 @@
+echo 'current workspace:'
+echo $WORKSPACE
+cd $WORKSPACE
+cd src/main/webapp
+
+npm set registry https://registry.npm.taobao.org && \
+npm set disturl https://npm.taobao.org/dist && \
+npm set sass_binary_site https://npm.taobao.org/mirrors/node-sass && \
+npm set electron_mirror https://npm.taobao.org/mirrors/electron && \
+npm set puppeteer_download_host https://npm.taobao.org/mirrors && \
+npm set chromedriver_cdnurl https://npm.taobao.org/mirrors/chromedriver && \
+npm set operadriver_cdnurl https://npm.taobao.org/mirrors/operadriver && \
+npm set phantomjs_cdnurl https://npm.taobao.org/mirrors/phantomjs && \
+npm set selenium_cdnurl https://npm.taobao.org/mirrors/selenium && \
+npm set node_inspector_cdnurl https://npm.taobao.org/mirrors/node-inspector && \
+npm cache clean --force
+
+echo 'current npm registry:'
+npm get registry
+npm install
+npm run build
+
+cd $WORKSPACE
+chmod +x mvnw
+./mvnw clean package -V -e
+./mvnw spring-boot:run
